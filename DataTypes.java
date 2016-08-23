6e2b1f5a85c1b2d3ee5437fb4cfb6b3ddb616a31
akash <akashkrishnan705@gmail.com>
2016-08-23 20:14:12 +0530

    Test1

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+        int k = 12;
+        double j = 4.0;
+        s = s.concat("is the best place to learn and practice coding!");
+        System.out.println(i+k);
+        System.out.println(d+j);
+        System.out.println(s);
+        /* Read and save an integer, double, and String to your variables.*/
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        scan.close();
+    	}
+	
+	}
+
